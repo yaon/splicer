1d594cbf7625fda03dd5277bf97d4710fc8e0747
Jim <caillardjim@gmail.com>
2015-10-19 19:17:52 +0200

    Add csv parsing and error handling

--- splicer.py
+++ splicer.py
@@ -3,41 +3,62 @@
 import subprocess
 import os
 from decimal import *
+import csv
 
-segments=sorted(os.listdir('segments'))
-segments_dir='segments'
-tmp_dir='tmp'
+# Args
+data_csv='data.csv'
+segments_dir='segments' + os.path.sep
+tmp_dir='tmp' + os.path.sep
+ffmpeg = 'ffmpeg'
 
 def call (cmd):
-    print(cmd)
     proc = subprocess.Popen(cmd, shell=True)
     proc.wait()
     if (proc.returncode):
-        raise "Error " + proc.returncode
+        raise 'Error ' + proc.returncode + ' ' + cmd
 
-def convert_ts (i, o):
-    call('ffmpeg -i '+ i +' -c copy -bsf:v h264_mp4toannexb -f mpegts ' + o)
+def check_io(i, o, debug):
+    i_exists = os.path.isfile(i)
+    o_exists = os.path.isfile(o)
+    if not i_exists: print('No input file, skipping '+debug)
+    elif o_exists: print('Output exists, skipping '+debug)
+    return i_exists and not o_exists
 
 def cut_video (i, o, start, duration):
-    i = i.replace(' ', '\\ ');
-    call('ffmpeg -i '+i+' -ss '+start+' -t '+duration+' -c:v libx264 -c:a aac -strict experimental -b:a 128k '+o)
+    debug = 'cut_video '+str(i)+' '+str(o)+' '+str(start)+' '+str(duration)
+    if check_io (i, o, debug):
+        call(ffmpeg+' -i '+i+' -ss '+start+' -t '+duration+' -c:v libx264 -c:a aac -strict experimental -b:a 128k '+o)
+
+def convert_ts (i, o):
+    debug = 'convert_ts '+str(i)+' '+str(o)
+    if check_io (i, o, debug):
+        call(ffmpeg + ' -i ' + i + ' -c copy -bsf:v h264_mp4toannexb -f mpegts ' + o)
 
 def concat_videos (videos, o):
-    print(videos)
-    cll('ffmpeg -i "concat:'+('|'.join(videos))+'" -c copy -bsf:a aac_adtstoasc '+o)
+    # let ffmpeg ask for replacement in this case
+    call(ffmpeg+' -i "concat:'+('|'.join(videos))+'" -c copy -bsf:a aac_adtstoasc '+o)
 
+class segment_data:
+    def __init__(self, data):
+        self.name, self.character, self.comment, self.runner, self.start, self.end, self.duration, self.video = data
 
+data = []
 ts_list = []
-try:
-    os.stat(tmp_dir)
-except:
-    os.mkdir(tmp_dir)
 
-for segment in segments:
-    number, name, start, stop = os.path.splitext(segment)[0].split()
-    duration = Decimal(stop)-Decimal(start)
-    cut_video(segments_dir+'/'+segment, tmp_dir+'/'+number+'.mp4', start, str(duration))
-    convert_ts(tmp_dir+'/'+number+'.mp4', tmp_dir+'/'+number+'.ts')
-    ts_list.append(tmp_dir+'/'+number+'.ts')
+try: os.stat(tmp_dir)
+except: os.mkdir(tmp_dir)
 
-concat_videos(ts_list, 'out.mp4')
+# Parse csv and fill data
+with open('data.csv', 'rb') as csvfile:
+    for row in csv.reader(csvfile, delimiter=',', quotechar='"'):
+        data.append(segment_data(row[:8]))
+
+# Cut videos and convert to ts
+for segment in data:
+    cut_video(segments_dir + segment.name + '.mp4', tmp_dir + segment.name + '.mp4', segment.start, segment.duration)
+    convert_ts(tmp_dir + segment.name + '.mp4', tmp_dir + segment.name + '.ts')
+    if os.path.isfile(tmp_dir + segment.name + '.ts'):
+        ts_list.append(tmp_dir + segment.name + '.ts')
+
+if len(ts_list):
+    concat_videos(ts_list, 'out.mp4')
